3b040c12eb5cde965c436a0f4e796b48caa01993
James Knott <jknott.terraform@gmail.com>
2020-08-07 07:31:30 -0500

    Testing var region to see if its broke

--- main.tf
+++ main.tf
@@ -130,7 +130,7 @@ resource "aws_security_group" "bastion" {
 }
 
 resource "aws_instance" "bastion" {
-  ami                         = var.bastion_ami[var.region]
+  ami                         = var.bastion_ami["us-west-1"]
   instance_type               = var.bastion_instance_type
   key_name                    = var.key_name
   monitoring                  = true
