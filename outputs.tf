f0796e992123bf845e1fcf3a5ef6fabeff147b23
James Knott <jknott.terraform@gmail.com>
2020-08-08 09:21:08 -0500

    already present removed changes

--- outputs.tf
+++ outputs.tf
@@ -46,13 +46,4 @@ output "vpc_id" {
    value = aws_vpc.environment.default_security_group_id
  }
 
-output "public_dns" {
-  description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = bastion.public_dns
-}
-
-output "public_ip" {
-  description = "List of public IP addresses assigned to the instances, if applicable"
-  value       = bastion.public_ip
-}
 
